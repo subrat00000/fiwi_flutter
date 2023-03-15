f9005634c47df56a060f354d32818223e4f6408b
subrat00000 <49115618+subrat00000@users.noreply.github.com>
2023-03-15 17:35:31 +0530

    auth guard

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fiwi/cubits/google_signin/google_signin_cubit.dart';
-import 'package:fiwi/cubits/google_signin/phone_signin_state.dart';
 import 'package:fiwi/cubits/internet_cubit.dart';
 import 'package:fiwi/cubits/phone_signin/phone_signin_cubit.dart';
+import 'package:fiwi/cubits/phone_signin/phone_signin_state.dart';
 import 'package:fiwi/routers.dart';
 import 'package:fiwi/view/home_screen.dart';
 import 'package:fiwi/view/splash_screen.dart';
@@ -15,57 +16,18 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => PhoneSigninCubit(),
-        ),
-        BlocProvider(
-          create: (context) => GoogleSigninCubit(),
-        ),
-      ],
-      child: MaterialApp(
-        home: BlocBuilder<PhoneSigninCubit, AuthState>(
-          buildWhen: (oldState, newState) {
-            return oldState is AuthInitialState;
-          },
-          builder: (context, state) {
-            if (state is AuthUserCreateState) {
-              return const HomeScreen();
-            } else {
-              return const SplashScreen();
-            }
-          },
-        ),
-        onGenerateRoute: Routers.generateRoute,
-        initialRoute: '/splash',
-      ),
+    return MaterialApp(
+      onGenerateRoute: Routers.generateRoute,
+      initialRoute: _auth.currentUser != null ? '/home' : '/splash',
     );
   }
 }
-// BlocConsumer<InternetCubit, InternetState>(
-//                     listener: (context, state) {
-//       if (state == InternetState.gained && internet == false) {
-//         ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-//           content: Text("Internet conntected"),
-//           backgroundColor: Colors.green,
-//           behavior: SnackBarBehavior.floating,
-//         ));
-//         internet = true;
-//       } else if (state == InternetState.lost) {
-//         ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-//           content: Text("No Internet Connection found"),
-//           backgroundColor: Colors.red,
-//           behavior: SnackBarBehavior.floating,
-//         ));
-//         internet = false;
-//       }
-//     }, builder: (context, state) {
